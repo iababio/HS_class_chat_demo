7deee38285de2bcfb01c5cfa69dc34a41d373c2f
Boakye I. Ababio <iba-melch@hotmail.com>
2026-02-21 19:39:55 +0000

    changing my name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
    <main className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col items-center p-8">
      <div className="w-full max-w-4xl">
      <h1 className="mb-8 text-6xl font-bold text-center">
-          <span className="bg-gradient-to-r from-red-600 to-cyan-600
+          👩🏻‍🌾👩🏻‍🌾<span className="bg-gradient-to-r from-red-600 to-cyan-600
           bg-clip-text text-transparent">Ricky pet-care </span>
       </h1>
        <div className="bg-white rounded-2xl shadow-xl p-6">
